06547fd78c444837b0bdb9c91e603005c1bf2e4d
Spencer Couture <spence.couture@gmail.com>
2024-08-11 11:49:08 -0400

    changed start-docker script to reference custom container

--- start_docker.sh
+++ start_docker.sh
@@ -2,7 +2,7 @@
 set -e
 
 # start client container
-docker run --rm -it -d --dns 10.0.1.4 --network=none -v "/tmp/quiche-server-data-$1":/server-data --name quiche-client cloudflare/quiche /bin/bash
+docker run --rm -it -d --dns 10.0.1.4 --network=none -v "/tmp/quiche-server-data-$1":/server-data --name quiche-client scouture/protobuf-quiche /bin/bash
 #docker run --shm-size=4g --rm -it -d  --network=none --entrypoint "/bin/bash" -v "/tmp/browsertime-$1":/browsertime --dns 10.0.1.4 --cap-add=SYS_NICE --name $1-browsertime constantin/browsertime
 DPID=$(docker inspect -f '{{.State.Pid}}' quiche-client)
 mkdir -p /var/run/netns
@@ -11,7 +11,7 @@ ln -s /proc/$DPID/ns/net /var/run/netns/quiche-client
 # docker run -e SSLKEYLOGFILE=/sslkeyfile quiche-client
 
 # start server container
-docker run --rm -it -d --dns 10.0.1.4 --network=none -v "/tmp/quiche-server-data-$1":/server-data --name quiche-server cloudflare/quiche /bin/bash
+docker run --rm -it -d --dns 10.0.1.4 --network=none -v "/tmp/quiche-server-data-$1":/server-data --name quiche-server scouture/protobuf-quiche /bin/bash
 #docker run --shm-size=4g --rm -it -d  --network=none --entrypoint "/bin/bash" -v "/tmp/browsertime-$1":/browsertime --dns 10.0.1.4 --cap-add=SYS_NICE --name $1-browsertime constantin/browsertime
 DPID=$(docker inspect -f '{{.State.Pid}}' quiche-server)
 mkdir -p /var/run/netns
